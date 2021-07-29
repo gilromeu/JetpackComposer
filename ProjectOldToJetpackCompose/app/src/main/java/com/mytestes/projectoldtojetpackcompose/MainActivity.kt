52618b24dd2c1e94cd7402db921f702824ee127e
Gil Romeu <gilromeu.p@gmail.com>
2021-07-29 19:22:14 -0300

    Implementação dos componentes

--- ProjectOldToJetpackCompose/app/src/main/java/com/mytestes/projectoldtojetpackcompose/MainActivity.kt
+++ ProjectOldToJetpackCompose/app/src/main/java/com/mytestes/projectoldtojetpackcompose/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.mytestes.projectoldtojetpackcompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mytestes.projectoldtojetpackcompose.ui.theme.MyApp
+import com.mytestes.projectoldtojetpackcompose.ui.theme.MyScreenContent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MyApp {
+                MyScreenContent()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, name = "Text preview")
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MyScreenContent()
     }
 }
